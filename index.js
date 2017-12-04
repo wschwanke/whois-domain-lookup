0cc8bed0f32ae5366962a84ebd1cabc2b38250bc
Will Schwanke <wschwanke@gmail.com>
2017-12-04 16:45:59 -0600

    Remove throw new from csvParser and just user console.error.

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ const csvParse = require('csv-parse');
 
 let delimiter = ',' || argv.n;
 let domainColName = 'Domain' || argv.d;
+const regExp = /^Name Server.+?$/gm;
+let nameServers = ''
+
 
 // csvParser calls the csvParse function from csv-parse node module. The default delimiter is `,`,
 // setting columns to true will return an array of objects with the key populated as the name of the column.
 const csvParser = csvParse({ delimiter: ',', columns: true }, (err, data) => {
   // We set index to 1 instead of 0 because at index 0 we have the CSV headers
   for (let csvRow = 1; csvRow < data.length; csvRow++) {
+    // We do a setTimeout becaue there is a limit to how often we can get info from the whois database
     setTimeout(() => {
       whois.lookup(data[csvRow][domainColName], (err, whois) => {
-        console.error('Error: ', err);
-        console.log('Raw Data: ', whois);
+        if (err) {
+          // If there is an error display it in the console
+          console.error('Error: ', err);
+        }
+        
       });
     }, csvRow * 100);
   }
